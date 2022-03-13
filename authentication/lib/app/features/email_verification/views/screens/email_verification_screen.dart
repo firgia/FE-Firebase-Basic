05a34f0b1d2a3081e6fb35f1aa016d39af41a7a9
flutterwithgia <flutterwithgia@gmail.com>
2022-03-13 11:33:15 +0700

    add design

--- authentication/lib/app/features/email_verification/views/screens/email_verification_screen.dart
+++ authentication/lib/app/features/email_verification/views/screens/email_verification_screen.dart
@@ -1,5 +1,8 @@
 library email_verification;
 
+import 'package:authentication/app/constans/app_constants.dart';
+import 'package:authentication/app/shared_components/async_button.dart';
+import 'package:authentication/app/shared_components/header_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,15 +12,58 @@ part '../../bindings/email_verification_binding.dart';
 // controller
 part '../../controllers/email_verification_controller.dart';
 
+// component
+part '../components/button/sign_out_button.dart';
+part '../components/button/resend_button.dart';
+part '../components/text/info_text.dart';
+
 class EmailVerificationScreen extends GetView<EmailVerificationController> {
   const EmailVerificationScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Email"),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultSpacing * 2),
+          child: Column(
+            children: [
+              const Spacer(),
+              _buildIllustration(),
+              const SizedBox(height: kDefaultSpacing * 4),
+              const HeaderText("Email Verification"),
+              const SizedBox(height: kDefaultSpacing),
+              const _InfoText(email: "test@gmail.com"),
+              const Spacer(),
+              _buildSignOutButton(),
+              const SizedBox(height: kDefaultSpacing),
+              _buildResendButton(),
+              const SizedBox(height: kDefaultSpacing),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _buildIllustration() {
+    return Image.asset(
+      ImageRasterPath.email,
+      height: 200,
+    );
+  }
+
+  Widget _buildSignOutButton() {
+    return _SignOutButton(
+      isLoading: false,
+      onPressed: () {},
+    );
+  }
+
+  Widget _buildResendButton() {
+    return _ResendButton(
+      onPressed: () {},
+      isLoading: false,
     );
   }
 }
