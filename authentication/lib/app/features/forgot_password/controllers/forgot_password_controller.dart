a9177781e188de1386d82aaa25a25193de9454bc
flutterwithgia <flutterwithgia@gmail.com>
2022-03-15 11:06:30 +0700

    add design

--- authentication/lib/app/features/forgot_password/controllers/forgot_password_controller.dart
+++ authentication/lib/app/features/forgot_password/controllers/forgot_password_controller.dart
@@ -1,3 +1,8 @@
 part of forgot_password;
 
-class ForgotPasswordController extends GetxController {}
+class ForgotPasswordController extends GetxController {
+  final email = TextEditingController();
+  final isLoading = false.obs;
+
+  void submit() async {}
+}
--- authentication/lib/app/features/forgot_password/views/screens/forgot_password_screen.dart
+++ authentication/lib/app/features/forgot_password/views/screens/forgot_password_screen.dart
@@ -1,5 +1,8 @@
 library forgot_password;
 
+import 'package:authentication/app/constans/app_constants.dart';
+import 'package:authentication/app/shared_components/async_button.dart';
+import 'package:authentication/app/shared_components/header_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,14 +12,84 @@ part '../../bindings/forgot_password_binding.dart';
 // controller
 part '../../controllers/forgot_password_controller.dart';
 
+// component
+part '../components/button/submit_button.dart';
+part '../components/text_field/email_text_field.dart';
+
 class ForgotPasswordScreen extends GetView<ForgotPasswordController> {
   const ForgotPasswordScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("forgot password"),
+    return Scaffold(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: SizedBox(
+              height: Get.height,
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: kDefaultSpacing * 2),
+                  child: Column(
+                    children: [
+                      const Spacer(),
+                      _buildIllustration(),
+                      const SizedBox(height: kDefaultSpacing),
+                      _buildTitle(),
+                      const SizedBox(height: kDefaultSpacing * 1.5),
+                      _buildInfo(),
+                      const Spacer(),
+                      _EmailTextField(controller: controller.email),
+                      const Spacer(),
+                      _buildSubmitButton(),
+                      const Spacer(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(kDefaultSpacing / 2),
+              child: BackButton(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIllustration() {
+    return Image.asset(
+      ImageRasterPath.lock,
+      height: 200,
+    );
+  }
+
+  Widget _buildTitle() {
+    return const Align(
+      alignment: Alignment.topLeft,
+      child: HeaderText("Forgot\nPassword ?"),
+    );
+  }
+
+  Widget _buildInfo() {
+    return Text(
+      "Don't worry! Please enter the email address associated with your account.",
+      style: TextStyle(
+        fontSize: 15,
+        color: Colors.grey[700],
+      ),
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return Obx(
+      () => _SubmitButton(
+        isLoading: controller.isLoading.value,
+        onPressed: () => controller.submit(),
       ),
     );
   }
